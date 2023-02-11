4dfc321d3db5b106e1a6f567bdd65486019b0850
SamPete <43476046+SamPete@users.noreply.github.com>
2023-02-10 22:05:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 #New Section to display fruitivice api response
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+
